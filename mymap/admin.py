35275993970fb27f3f8160376985820960eec140
shadeofpurple79 <>
2023-05-27 18:11:55 +0000

    Undo and comment out all the recent changes to make admin work again

--- mymap/admin.py
+++ mymap/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Destination
-from .models import Food
+# from .models import Destination
+# from .models import Food
 
-admin.site.register(Destination)
-admin.site.register(Food)
+# admin.site.register(Destination)
+# admin.site.register(Food)
--- mymap/models.py
+++ mymap/models.py
@@ -3,31 +3,31 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 
-class Destination(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    city = models.CharField(max_length=200, unique=True)
-    country = models.CharField(max_length=200)
-    month_visited = models.DateField
-    year_visited = models.DateField
-    month_tovisit = models.DateField
-    year_to_visit = models.DateField
-    my_experience = models.BooleanField
-    destination_image = CloudinaryField('image', default='placeholder')
+# class Destination(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     city = models.CharField(max_length=200, unique=True)
+#     country = models.CharField(max_length=200)
+#     month_visited = models.DateField
+#     year_visited = models.DateField
+#     month_tovisit = models.DateField
+#     year_to_visit = models.DateField
+#     my_experience = models.BooleanField
+#     destination_image = CloudinaryField('image', default='placeholder')
 
-    # class Meta:
-    #     ordering = ['-created_on']
+# class Meta:
+#     ordering = ['-created_on']
 
-    # def _str_(self):
-    #     return self.title
+# def _str_(self):
+#     return self.title
 
 
-class Food(models.Model):
-    destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
-    name_of_food = models.CharField(max_length=200, unique=True)
-    food_image = CloudinaryField('image', default='placeholder')
+# class Food(models.Model):
+#     destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
+#     name_of_food = models.CharField(max_length=200, unique=True)
+#     food_image = CloudinaryField('image', default='placeholder')
 
-    # class Meta:
-    #     ordering = ['-created_on']
+# class Meta:
+#     ordering = ['-created_on']
 
-    # def _str_(self):
-    #     return self.title
+# def _str_(self):
+#     return self.title
